3ce5b2c99822f2662309494707bc24cb02d6580b
jettro.coenradie <jettro.coenradie@f430e863-351a-0410-99cc-21b2beb4a24d>
2006-08-10 20:39:53 +0000

    initial version

---
+++ SpringframeworkArchetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $nl.gridshore.samples.archetype.springframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
